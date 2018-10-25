4fa92b68f3b0bb603a127a2798108e63a284f1f1
GLOBANT\andres.vargas <andres.vargas@globant.com>
2018-10-24 19:17:49 -0500

    TDD Ciclo ROJO creacion de los casos de prueba para la consulta de comentarios asociados al recurso

---
+++ resourcesApp/tests/test_get_omentarios.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+
+from resourcesApp.models import Recurso
+
+
+class TestComentario(TestCase):
+
+    def test_recurso_sin_comentarios(self):
+        tester = Recurso.object.get(nombre="prueba_uno");
+        self.assertEqual(len(tester.comentarios), 0)
+
+
+    def test_recurso_un_comentarios(self):
+        tester = Recurso.object.get(nombre="prueba_dos");
+        self.assertEqual(len(tester.comentarios), 1)
+        self.assertEqual( not tester.comentarios[0], False)
+
+    def test_varios_comentarios(self):
+        tester = Recurso.object.get(nombre="prueba_tres");
+        self.assertEqual(len(tester.comentarios), 3)
+        self.assertEqual(not tester.comentarios[0], False)
+        self.assertEqual(not tester.comentarios[1], False)
+        self.assertEqual(not tester.comentarios[2], False)
+
+
