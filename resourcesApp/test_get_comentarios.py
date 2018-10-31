1ea8665b420064489abd412d874cd91a6dc8444a
adriana bonilla <apbonillab@gmail.com>
2018-10-30 22:26:17 -0500

    TDD Verde comentarios

--- resourcesApp/test_get_comentarios.py
+++ resourcesApp/test_get_comentarios.py
@@ -1,25 +1,26 @@
 from unittest import TestCase
 
+from resourcesApp.Views.RecursoViews import recursos_comentarios
 from resourcesApp.models import Recurso
 
 
 class TestComentario(TestCase):
 
     def test_recurso_sin_comentarios(self):
-        tester = Recurso.object.get(nombre="prueba_uno");
-        self.assertEqual(len(tester.comentarios), 0)
+        tester = recursos_comentarios;
+        self.assertEqual(len(tester.comentario), 0)
 
 
     def test_recurso_un_comentarios(self):
-        tester = Recurso.object.get(nombre="prueba_dos");
-        self.assertEqual(len(tester.comentarios), 1)
-        self.assertEqual( not tester.comentarios[0], False)
+        tester = recursos_comentarios(1);
+        self.assertEqual(len(tester.comentario), 1)
+        self.assertEqual( not tester.comentario[0], False)
 
     def test_varios_comentarios(self):
-        tester = Recurso.object.get(nombre="prueba_tres");
-        self.assertEqual(len(tester.comentarios), 3)
-        self.assertEqual(not tester.comentarios[0], False)
-        self.assertEqual(not tester.comentarios[1], False)
-        self.assertEqual(not tester.comentarios[2], False)
+        tester = recursos_comentarios(2);
+        self.assertEqual(len(tester.comentario), 3)
+        self.assertEqual(not tester.comentario[0], False)
+        self.assertEqual(not tester.comentario[1], False)
+        self.assertEqual(not tester.comentario[2], False)
 
 
--- resourcesApp/tests.py
+++ resourcesApp/tests.py
@@ -4,7 +4,3 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 # Create your tests here.
-
-
-def test_recurso_sin_comentarios(self):
-    print("entro")
